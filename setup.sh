5480ea5d236156a75f2993c97e8f8fbc209d182a
Prashanth <pdharamsoth@monarchtractor.com>
2024-11-04 22:14:01 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+
+echo "Creating .env file..."
+echo "REACT_APP_GOOGLE_API_KEY=AIzaSyAGQO61kStlznQU8mXYmxtoG8qMw3QKc" >> .env
+echo "REACT_APP_SEARCH_ENGINE_ID=a5dc6eb4869864054" >> .env
+echo "REACT_APP_HUGGINGFACE_API_KEY=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" >> .env
+echo ".env file created successfully!"
+
